fde2cccd0e40626adbd718c6e8b6515d6169e757
Ngoc Thao <tranthaihaong1707@gmail.com>
2025-05-18 09:51:18 +0700

    refactor: update HTML structure and add Firebase configuration

---
+++ templatemo_589_lugx_gaming/assets/js/firebase-config.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiLrnJC3U1y5uq5nLO5y_91mvHY_3aRuE",
+  authDomain: "game-demo-1707.firebaseapp.com",
+  projectId: "game-demo-1707",
+  storageBucket: "game-demo-1707.firebasestorage.app",
+  messagingSenderId: "185489419922",
+  appId: "1:185489419922:web:ea13d1425abe767d92c04b",
+  measurementId: "G-FDJMVKPHF2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+
+const querySnapshot = await getDocs(collection(db, "game"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
