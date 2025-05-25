779aca2f8949124239dd443564b0d82bddf56f0a
Ke Dat <datmartin10@gmail.com>
2025-05-25 09:15:39 +0700

    Display list product to homepage

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -10,7 +10,7 @@ import {
   getDocs,
   doc,
   getDoc,
-} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js"; 
+} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,8 +31,43 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 // Initialize Cloud Firestore and get a reference to the service
 
-const querySnapshot = await getDocs(collection(db, "game"));
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, " => ", doc.data());
-});
+async function displayProducts() {
+  const productCollection = collection(db, "game");
+  const querySnapshot = await getDocs(productCollection);
+
+  const wrapper = document.querySelector(".trending .container .row");
+
+  querySnapshot.forEach((doc) => {
+    const product = doc.data();
+    console.log("🚀 ~ querySnapshot.forEach ~ product:", product)
+
+    // Create a swiper-slide element
+    const slide = document.createElement("div");
+    slide.classList.add("col-md-6");
+
+    // Add product content to the slide
+    slide.innerHTML = `
+                  <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="${product.thumbnail}" alt="" /></a>
+             <span class="price"><em>${product.Price}$</em>${product.salePrice
+      }$</span>
+            </div>
+            <div class="down-content">
+              <span class="category">${product.category}</span>
+              <h4>${product.Name}</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+    `;
+
+    // Append the slide to the swiper-wrapper
+    wrapper.appendChild(slide);
+  });
+
+  // Reinitialize Swiper after adding slides
+
+}
+
+// Call the function to display products
+displayProducts();
